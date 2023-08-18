e87dea7765ad916b21f136b4fa58126bf550b1d5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-08-18 08:30:02 +0100

    pydocstyle

--- dof_transformations.py
+++ dof_transformations.py
@@ -6,4 +6,12 @@ import symfem
 def compute_base_transformations(
     element: symfem.finite_element.CiarletElement
 ) -> typing.Dict[str, sympy.matrices.dense.MutableDenseMatrix]:
+    """Compute the base transformations for an element.
+
+    Args:
+        element: The element
+
+    Returns:
+        A dictionary of base transformation matrices
+    """
     raise NotImplementedError()
