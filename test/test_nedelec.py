c1449e70039efea199f115db62769c023bcd1fb7
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-05-08 09:02:39 +0100

    correct degrees in tests

--- test/test_nedelec.py
+++ test/test_nedelec.py
@@ -7,18 +7,20 @@ from dof_transformations import compute_base_transformations
 
 @pytest.mark.parametrize("cell_type", ["triangle", "quadrilateral"])
 def test_nedelec_2d(cell_type):
-    """Test that the transformations are correct for degree 3 Nedelec1 on 2D cells."""
-    element = symfem.create_element(cell_type, "Nedelec", 3)
+    """Test that the transformations are correct for degree 2 Nedelec1 on 2D cells."""
+    element = symfem.create_element(cell_type, "Nedelec", 2)
     t = compute_base_transformations(element)
     assert len(t) == 1
+    print(t["interval reflection"])
+    print([[0, -1, 0], [-1, 0, 0], [0, 0, -1]])
     assert t["interval reflection"] == sympy.Matrix(
         [[0, -1, 0], [-1, 0, 0], [0, 0, -1]]
     )
 
 
 def test_nedelec_tetrahedron():
-    """Test that the transformations are correct for degree 3 Nedelec1 on a tetrahedron."""
-    element = symfem.create_element("tetrahedron", "Nedelec", 3)
+    """Test that the transformations are correct for degree 2 Nedelec1 on a tetrahedron."""
+    element = symfem.create_element("tetrahedron", "Nedelec", 2)
     t = compute_base_transformations(element)
     assert len(t) == 3
     assert t["interval reflection"] == sympy.Matrix(
@@ -47,8 +49,8 @@ def test_nedelec_tetrahedron():
 
 
 def test_nedelec_hexahedron():
-    """Test that the transformations are correct for degree 3 Nedelec1 on a hexahedron."""
-    element = symfem.create_element("hexahedron", "Nedelec", 2)
+    """Test that the transformations are correct for degree 1 Nedelec1 on a hexahedron."""
+    element = symfem.create_element("hexahedron", "Nedelec", 1)
     t = compute_base_transformations(element)
     assert len(t) == 3
     assert t["interval reflection"] == sympy.Matrix([[0, -1], [-1, 0]])
